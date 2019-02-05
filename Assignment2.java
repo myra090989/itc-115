7764283441645c24e18b6fc48a5427a842539724
myra090989 <33969068+myra090989@users.noreply.github.com>
2019-02-04 19:50:37 -0800

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,18 @@
+
+
+class Assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int m = 1;
+		int mm = 1;
+		
+		for (int i = 0; i <= 10; i++) {
+			System.out.println(m);
+		m = m + mm;
+			mm = m - mm;
+
+		}
+	}
+
+}
